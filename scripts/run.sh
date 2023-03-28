bb263bb5ab433b289ffc56eb3baa4583d40b2687
berkid89 <berkid89@gmail.com>
2023-03-28 10:09:14 +0200

    extre steps

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,13 @@
 cd /root/www/Silo
 lsof nohup.out
 kill -9 123
+cp /home/temp/silo/* /root/www/Silo
 nohup dotnet MessageSilo.Silo.dll &
 cat nohup.out
 
 cd /root/www/API
 lsof nohup.out
 kill -9 123
+cp /home/temp/api/* /root/www/API
 nohup dotnet MessageSilo.API.dll --urls "https://*:443" &
 cat nohup.out
