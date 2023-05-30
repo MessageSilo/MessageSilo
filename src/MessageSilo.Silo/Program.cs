71e504b53bc301af7d0deaa38f1315e4f48c0e4b
berkid89 <berkid89@gmail.com>
2023-05-30 12:00:25 +0200

    use env vars

--- src/MessageSilo.Silo/Program.cs
+++ src/MessageSilo.Silo/Program.cs
@@ -13,6 +13,7 @@ using System.Net;
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", false, true)
     .AddJsonFile("appsettings.Development.json", true, true)
+    .AddEnvironmentVariables()
     .Build();
 
 var siloIP = IPAddress.Parse(configuration["Orleans:PrimarySiloAddress"]);
