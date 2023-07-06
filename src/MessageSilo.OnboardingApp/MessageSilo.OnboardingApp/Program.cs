29b09657977946244c3c7922aee784934b6355bd
berkid89 <berkid89@gmail.com>
2023-07-06 07:13:45 +0200

    wip

---
+++ src/MessageSilo.OnboardingApp/MessageSilo.OnboardingApp/Program.cs
@@ -0,0 +1,45 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+AddBlazorise(builder.Services);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+});
+
+app.Run();
+
+
+void AddBlazorise(IServiceCollection services)
+{
+    services
+        .AddBlazorise();
+    services
+        .AddBootstrap5Providers()
+        .AddFontAwesomeIcons();
+}
