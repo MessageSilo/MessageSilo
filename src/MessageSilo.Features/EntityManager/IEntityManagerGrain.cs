3649a49a4d0f6fc5e4d4f4251c0f45a000768f62
berkid89 <berkid89@gmail.com>
2023-08-08 08:11:37 +0200

    count thorughput in kb per month

--- src/MessageSilo.Features/EntityManager/IEntityManagerGrain.cs
+++ src/MessageSilo.Features/EntityManager/IEntityManagerGrain.cs
@@ -1,11 +1,6 @@
 ﻿using FluentValidation.Results;
 using MessageSilo.Shared.Models;
 using Orleans;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MessageSilo.Features.EntityManager
 {
@@ -16,5 +11,9 @@ namespace MessageSilo.Features.EntityManager
         Task<List<ValidationFailure>?> Upsert(Entity entity);
 
         Task<List<ValidationFailure>?> Delete(string entityName);
+
+        Task IncreaseUsedThroughput(string messageBody);
+
+        Task<double> GetUsedThroughput(int date);
     }
 }
