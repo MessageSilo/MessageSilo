95a14b34b8c87afd4241f57da92a0a723ec35777
Daniel Berki <berkid89@gmail.com>
2024-09-04 20:33:39 +0200

    WIP: Diagram

---
+++ src/MessageSilo.Client/Models/ConnectionNode.cs
@@ -0,0 +1,16 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+using MessageSilo.Application.DTOs;
+
+namespace MessageSilo.Client.Models
+{
+    public class ConnectionNode : NodeModel
+    {
+        public ConnectionSettingsDTO DTO { get; set; }
+
+        public ConnectionNode(ConnectionSettingsDTO dto, string name, Point? position = null) : base(name, position)
+        {
+            DTO = dto;
+        }
+    }
+}
---
+++ src/MessageSilo.Client/Models/TargetNode.cs
@@ -0,0 +1,16 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+using MessageSilo.Application.DTOs;
+
+namespace MessageSilo.Client.Models
+{
+    public class TargetNode : NodeModel
+    {
+        public TargetDTO DTO { get; set; }
+
+        public TargetNode(TargetDTO dto, string name, Point? position = null) : base(name, position)
+        {
+            DTO = dto;
+        }
+    }
+}
